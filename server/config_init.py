44b87fabe14f7ffd6267c200e62f9f8a8fd6058a
Martu <mlozano@fi.uba.ar>
2025-04-16 20:34:18 -0300

    base del proyecto para python

---
+++ server/config_init.py
@@ -0,0 +1,30 @@
+from configparser import ConfigParser
+import os
+
+def initialize_config():
+    """ Parse env variables or config file to find program config params
+
+    Function that search and parse program configuration parameters in the
+    program environment variables first and the in a config file. 
+    If at least one of the config parameters is not found a KeyError exception 
+    is thrown. If a parameter could not be parsed, a ValueError is thrown. 
+    If parsing succeeded, the function returns a ConfigParser object 
+    with config parameters
+    """
+
+    config = ConfigParser(os.environ)
+    # If config.ini does not exists original config object is not modified
+    config.read("config.ini")
+
+    config_params = {}
+    try:
+        config_params["port"] = int(os.getenv('SERVER_PORT', config["DEFAULT"]["SERVER_PORT"]))
+        config_params["listen_backlog"] = int(os.getenv('SERVER_LISTEN_BACKLOG', config["DEFAULT"]["SERVER_LISTEN_BACKLOG"]))
+        config_params["logging_level"] = os.getenv('LOGGING_LEVEL', config["DEFAULT"]["LOGGING_LEVEL"])
+        config_params["agency_num"] = int(os.getenv('NUM_CLIENTS'))
+    except KeyError as e:
+        raise KeyError("Key was not found. Error: {} .Aborting server".format(e))
+    except ValueError as e:
+        raise ValueError("Key could not be parsed. Error: {}. Aborting server".format(e))
+
+    return config_params
