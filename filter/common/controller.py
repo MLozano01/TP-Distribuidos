29c3c1b1e5297850dd20c60e2066db392a89fc3c
Martu <mlozano@fi.uba.ar>
2025-04-21 18:16:02 -0300

    stuff

--- filter/common/controller.py
+++ filter/common/controller.py
@@ -40,6 +40,6 @@ class Controller:
     def stop(self):
         for filter in self.all_filters:
             if filter.is_alive():
-                filter.end_filter()
+                # filter.end_filter()
                 filter.join()
                 filter.terminate()
