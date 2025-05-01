f63a0bad3c3452ef26b9d6bb04f6db9022237e2e
Antonella Pellegrini <antopell29@gmail.com>
2025-05-01 23:27:05 +0000

    mejora del script de comparacion

--- results/compare-outputs.sh
+++ results/compare-outputs.sh
@@ -1,1 +1,6 @@
-python3 comparer.py $1 $2
+#!/bin/bash
+
+# (in results) ./compare-outputs.sh ratings_small client1 
+base="result_${1}.json"
+check="../data/results_${2}.json"
+python3 comparer.py "$base" "$check"
