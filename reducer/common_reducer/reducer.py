48fe0409e0677d5126e2713cd3d06f8c867f4855
Martu <mlozano@fi.uba.ar>
2025-06-22 19:06:25 -0300

    nuevo eof en reducer

--- reducer/common_reducer/reducer.py
+++ reducer/common_reducer/reducer.py
@@ -10,7 +10,7 @@ logging.getLogger("RabbitMQ").setLevel(logging.DEBUG)
 
 class Reducer:
 
-    def __init__(self, config, partial_result_backup, secuence_number_backup, state_manager: StatePersistence):
+    def __init__(self, config, partial_status_backup, secuence_number_backup, state_manager: StatePersistence):
         self.config = config
         self.queue_rcv = None
         self.queue_snd = None
@@ -18,9 +18,11 @@ class Reducer:
         self.query_id = config["query_id"]
         self.reduce_by = config['reduce_by']
 
-        self.partial_result = partial_result_backup
+        self.partial_status = partial_status_backup
         self.batches_seen = secuence_number_backup
         self._state_manager = state_manager
+
+        self.protocol = Protocol()
 
         # Setup signal handler for SIGTERM
         # signal.signal(signal.SIGTERM, self._handle_shutdown)
@@ -41,25 +43,21 @@ class Reducer:
 
     def reducer(self, data):
         try:
-            protocol = Protocol()
-
-            msg = self._get_msg(data, protocol)
+            msg = self._get_msg(data)
 
             if self._is_repeated(str(msg.client_id), str(msg.secuence_number)):
                 logging.info(f"Discading a repeated package of secuence number {msg.secuence_number}")
                 return
 
             if msg.finished:
-                logging.info(f"Finished msg received on query_id {self.query_id}")
-                self._handle_finished(str(msg.client_id), protocol)
+                logging.info(f"Finished msg received on query_id {self.query_id}, with final secuence number {msg.secuence_number}")
+                self.partial_status[str(msg.client_id)]["final_secuence"] = msg.secuence_number
+                self._save_info(str(msg.client_id), str(msg.secuence_number))
+                self._handle_finished(str(msg.client_id))
                 return
             
-            self.partial_result = parse_reduce_funct(data, self.reduce_by, self.partial_result, str(msg.client_id))
-            self.batches_seen[str(msg.client_id)].append(str(msg.secuence_number))
-
-            self._state_manager.save(self.partial_result)
-            self._state_manager.save_secuence_number_data(str(msg.secuence_number), str(msg.client_id))
-
+            self._manage_msg(data, str(msg.client_id), str(msg.secuence_number))
+            self._save_info(str(msg.client_id), str(msg.secuence_number))
 
         except json.JSONDecodeError as e:
             logging.error(f"Failed to decode JSON: {e}")
@@ -72,35 +70,54 @@ class Reducer:
         self.batches_seen.setdefault(client_id, [])
         return secuence_number in self.batches_seen[client_id]
 
+    def _save_info(self, client_id, secuence_number):
+        self._state_manager.save(self.partial_status)
+        self._state_manager.save_secuence_number_data(secuence_number, client_id)
+
+    def _manage_msg(self, data, client_id, secuence_number):
+        self.partial_status.setdefault(client_id, {"results": {}, "final_secuence": None})
+
+        self.partial_status[client_id]['results'] = parse_reduce_funct(data, self.reduce_by, self.partial_status[client_id]['results'])
+        self.batches_seen[client_id].append(secuence_number)
+
+        if self.partial_status[client_id]['final_secuence']:
+            logging.info("Got a delayed message")
+            self._handle_finished(client_id)
+
     def _clean_up(self, client_id):
-        self.partial_result.pop(client_id)
-        self._state_manager.save(self.partial_result)
+        self.partial_status.pop(client_id)
+        self._state_manager.save(self.partial_status)
 
         self.batches_seen.pop(client_id)
         self._state_manager.clean_client(client_id)
 
-    def _handle_finished(self, client_id, protocol):
-        result = parse_final_result(self.reduce_by, self.partial_result, client_id)
+    def _handle_finished(self, client_id):
         
-        res_proto = protocol.create_result(result, int(client_id))
+        if len(self.batches_seen[client_id]) != self.partial_status[client_id]["final_secuence"]:
+            logging.info(f'Not all messages have arrived = Batches Seen: {len(self.batches_seen[client_id])} vs Finished Secuence Number: {self.partial_status[str(client_id)]["final_secuence"]}')
+            return
+
+        result = parse_final_result(self.reduce_by, self.partial_status[client_id]['results'])
+        
+        res_proto = self.protocol.create_result(result, int(client_id))
         key = f'{self.config["routing_snd_key"]}_{client_id}'
         self.queue_snd.publish(res_proto, key)
 
         self._clean_up(client_id)
 
-        res_decoded = protocol.decode_result(res_proto)
+        res_decoded = self.protocol.decode_result(res_proto)
         logging.info(f"Final result: {res_decoded}")
-        
+
         return
 
-    def _get_msg(self, data, protocol):
-        aggr_msg = protocol.decode_aggr_batch(data)
+    def _get_msg(self, data):
+        aggr_msg = self.protocol.decode_aggr_batch(data)
         is_aggr_msg = (len(aggr_msg.aggr_row) > 0) or aggr_msg.finished
 
         if is_aggr_msg:
             msg = aggr_msg
         else:
-            msg = protocol.decode_movies_msg(data)
+            msg = self.protocol.decode_movies_msg(data)
 
         return msg
 
